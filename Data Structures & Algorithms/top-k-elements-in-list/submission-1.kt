91e622945ddceca49b6792784eae6a66b70ef083
Richard Wilkerson <r.christian.wilkerson@gmail.com>
2026-08-28 14:37:29 -0600

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+
+        // map each with the total appearances
+        val frequency = mutableMapOf<Int, Int>()
+
+        nums.forEach {
+            if (frequency.containsKey(it)) {
+                frequency[it] = frequency.getValue(it) + 1
+            } else frequency[it] = 1
+        }
+
+        // if the array does not have k number of options
+
+        // order the totals
+        return frequency
+            .entries // gives each entry with .key and .value
+            .sortedByDescending { it.value } // sort by value
+            .take(k) // take top k
+            .map { it.key } // keep only the key, not the value
+            .toIntArray()
+    }
+}
